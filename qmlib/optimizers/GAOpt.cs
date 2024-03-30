3da411b3a10daf40bd8e15220c499e0e07dd461c
fran <frannuca@gmail.com>
2024-03-30 13:38:55 +0100

    first commit with PoC GA algo

---
+++ qmlib/optimizers/GAOpt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.Genetic;
+
+namespace qmlib.optimizers
+{
+    public class GAOpt : IGenetic
+    {
+
+        public GAOpt(int population_size, IEnumerable<(double lowlimit, double highlimit)> variable_limits ,Func<double[], double> f_fitness): base(population_size, variable_limits.Count(), f_fitness) 
+        { 
+            limits = variable_limits.ToArray();
+        }
+
+        private readonly (double lowlimit, double highlimit)[] limits;
+
+        protected override double[] Scale(double[] x)
+        {
+            return x.Select((a, n) => limits[n].lowlimit + (limits[n].highlimit - limits[n].lowlimit)*a).ToArray();
+        }
+    }
+}
---
+++ qmlib/optimizers/IGenetic.cs
@@ -0,0 +1,58 @@
+﻿using Accord.Genetic;
+using Accord.Math.Random;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qmlib.optimizers
+{
+    abstract public class IGenetic
+    {
+        public IGenetic(int population_size, int chromosome_length, Func<double[],double> f_fitness)
+        {
+            PopulationSize = population_size;     
+            Fitness = new FitnessFunction(f_fitness);
+            ChromosomeLength = chromosome_length;
+        }
+
+        public int PopulationSize { get; }
+        public int ChromosomeLength { get; }
+        public FitnessFunction Fitness { get; }
+
+        public double[] Fit(int max_epochs)
+        {
+            ISelectionMethod selection = new EliteSelection();
+            var rndsource = new Accord.Statistics.Distributions.Univariate.UniformContinuousDistribution(0,1);
+            var chromo = new DoubleArrayChromosome(rndsource, rndsource, rndsource, ChromosomeLength);
+            var pop = new Population(PopulationSize, chromo, Fitness, selection);
+            return [0,0];
+        }
+
+        abstract protected double[] Scale(double[] x);
+    }
+
+    public class FitnessFunction : IFitnessFunction
+    {
+        public FitnessFunction(Func<double[], double> f_fitness) 
+        {
+            Fitness = f_fitness;
+        }
+
+        public Func<double[], double> Fitness { get; }
+
+        public double Evaluate(IChromosome chromosome)
+        {
+            var dchromosome = chromosome as DoubleArrayChromosome;
+            if(dchromosome == null)
+            {
+                throw new ArgumentException("Only double array chromosomes are expected");
+            }
+
+            return Fitness(dchromosome.Value);
+
+        }
+    }
+}
